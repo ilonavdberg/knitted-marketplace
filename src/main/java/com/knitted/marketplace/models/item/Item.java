998f54ad2b7a0dbe52542aa020f6cf87f8a6b581
ilonavdberg <137798906+ilonavdberg@users.noreply.github.com>
2024-12-23 23:02:17 +0100

    updated application properties for database and renamed items table

--- src/main/java/com/knitted/marketplace/models/item/Item.java
+++ src/main/java/com/knitted/marketplace/models/item/Item.java
@@ -1,11 +1,9 @@
 package com.knitted.marketplace.models.item;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name="items")
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
