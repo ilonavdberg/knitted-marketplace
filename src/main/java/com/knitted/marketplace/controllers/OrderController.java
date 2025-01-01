64fd789f82ebcdcf0769fd3c9df0d7280f9fc860
ilonavdberg <137798906+ilonavdberg@users.noreply.github.com>
2025-01-01 21:03:10 +0100

    incorporated create order method

--- src/main/java/com/knitted/marketplace/controllers/OrderController.java
+++ src/main/java/com/knitted/marketplace/controllers/OrderController.java
@@ -1,9 +1,7 @@
 package com.knitted.marketplace.controllers;
 
-import com.knitted.marketplace.exception.exceptions.InvalidStatusChangeException;
-import com.knitted.marketplace.models.item.Item;
-import com.knitted.marketplace.models.item.ItemStatus;
-import com.knitted.marketplace.services.ItemService;
+import com.knitted.marketplace.models.order.Order;
+import com.knitted.marketplace.services.OrderService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,20 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static com.knitted.marketplace.config.ApiConfig.BASE_URL;
 
+
 @RestController
 @RequestMapping(BASE_URL)
 public class OrderController {
-    private final ItemService itemService;
+    private final OrderService orderService;
 
-    public OrderController(ItemService itemService) {
-        this.itemService = itemService;
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
     }
 
     @PostMapping("items/{id}/order")
-    public void orderItem(@PathVariable Long id) {
-        Item item = itemService.getItem(id);
-        itemService.updateItemStatus(item.getId(), ItemStatus.SOLD);
-
-
+    public void orderItem(@PathVariable("id") Long itemId) {
+        Order order = orderService.orderItem(itemId);
     }
 }
