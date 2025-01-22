b09c2658a106c235c47106847e52bd48072c266d
ilonavdberg <137798906+ilonavdberg@users.noreply.github.com>
2025-01-22 12:37:14 +0100

    created UserServiceImpl class

--- src/main/java/com/knitted/marketplace/models/User.java
+++ src/main/java/com/knitted/marketplace/models/User.java
@@ -2,6 +2,8 @@ package com.knitted.marketplace.models;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name="users")
 public class User {
@@ -12,6 +14,27 @@ public class User {
     private String username;
     private String password;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles;
+
     @OneToOne(mappedBy = "user")
     private Contact contact;
+
+    //Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
 }
